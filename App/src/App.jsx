ed52a6202effc788b2ad9f337a3a484de06cdda4
david <david@securescan.com>
2022-12-15 14:20:07 -0500

    Add login and out buttons for auth0

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import Greeting from './Greeting.jsx';
 import PDFLink from './PDFLink.jsx';
+import LoginButton from './LoginButton.jsx';
+import LogoutButton from './LogoutButton.jsx';
+import Profile from './Profile.jsx';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -9,9 +13,23 @@ const root = createRoot(container);
 const page = (
   <div>
     <Greeting />
+    <Profile />
+    <hr />
+    <LoginButton />
+    <hr />
+    <LogoutButton />
+    <hr />
     <PDFLink />
   </div>
 );
-root.render(page);
+root.render(
+  <Auth0Provider
+    domain="dev-6xzvx6amw4huzdgq.us.auth0.com"
+    clientId="rcemq97nBaT1z5qWiuzy2JzeuE2qXoNI"
+    redirectUri="http://localhost:3000"
+  >
+    {page}
+  </Auth0Provider>
+);
 
 console.log('ESlint should warn about me here but not in the server.');
