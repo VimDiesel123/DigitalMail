d51876f2c2307f3eddbd3ca05dbbf09cae8506d6
david <david@securescan.com>
2023-01-05 16:19:01 -0500

    Spruce up NavBar a little

--- App/src/NavBar/NavBar.jsx
+++ App/src/NavBar/NavBar.jsx
@@ -1,26 +1,67 @@
 import React from 'react';
-import { Navbar, Container, Form, Nav, NavItem, NavLink, FormControl } from 'react-bootstrap';
+import {
+  Navbar,
+  Container,
+  Form,
+  Nav,
+  NavItem,
+  NavLink,
+  FormControl,
+  Badge,
+  Dropdown,
+} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCow, faInbox, faGear, faCircleInfo, faUser } from '@fortawesome/free-solid-svg-icons';
+import Search from './Search.jsx';
 
 export default function NavBar() {
   return (
-    <Navbar expand="lg" bg="secondary" variant="dark" fixed="top">
+    <Navbar expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand>Digital Mail</Navbar.Brand>
-        <Form>
-          <FormControl type="search" placeholder="Search" />
-        </Form>
+        <Navbar.Brand className="d-flex align-items-center">
+          <FontAwesomeIcon icon={faCow} size="xl" />
+          <span className="ms-3 display-4 fs-4">Digital Mail</span>
+        </Navbar.Brand>
+        <Search />
         <Nav>
           <NavItem>
-            <NavLink active>Inbox</NavLink>
+            <NavLink active>
+              <div className="position-relative">
+                <FontAwesomeIcon icon={faInbox} size="lg" />
+                <Badge
+                  bg="info"
+                  className="rounded-circle position-absolute top-0 end-0"
+                  style={{ marginTop: '-1px', marginRight: '-1px', padding: '.3rem' }}
+                >
+                  <span className="visually-hidden">Unread</span>
+                </Badge>
+              </div>
+            </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink>Settings</NavLink>
+            <NavLink>
+              <FontAwesomeIcon icon={faGear} size="lg" />
+            </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink>About</NavLink>
+            <NavLink>
+              <FontAwesomeIcon icon={faCircleInfo} size="lg" />
+            </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink>Profile</NavLink>
+            <NavLink>
+              <div className="d-flex">
+                <Dropdown align="end">
+                  <Dropdown.Toggle as="span" className="ms-1">
+                    <FontAwesomeIcon icon={faUser} size="lg" />
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Header>User Settings</Dropdown.Header>
+                    <Dropdown.Item>Profile or something</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+            </NavLink>
           </NavItem>
         </Nav>
       </Container>
