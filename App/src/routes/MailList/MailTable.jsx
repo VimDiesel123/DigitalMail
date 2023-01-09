f4682c96ad01881428355703fb533175cd52564e
david <david@securescan.com>
2023-01-09 13:58:17 -0500

    Add PropTypes to the MailTable

--- App/src/routes/MailList/MailTable.jsx
+++ App/src/routes/MailList/MailTable.jsx
@@ -8,6 +8,7 @@ import {
   faUpRightFromSquare,
 } from '@fortawesome/free-solid-svg-icons';
 import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons';
+import PropTypes from 'prop-types';
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
@@ -56,35 +57,58 @@ function MailItemRow({ link }) {
   );
 }
 
+MailItemRow.propTypes = {
+  link: PropTypes.string.isRequired,
+};
+
 export default function MailTable({ mail }) {
   const mailRows = mail
     ? mail.map((item) => <MailItemRow key={item.id} link={item.signedUrl} />)
     : null;
   return (
     <Table hover className="mt-3 mb-0" striped responsive>
-      <th className="text-bg-primary p-1 rounded-top" colSpan={4}>
-        <Form>
-          <Form.Group>
-            <Form.Check type="checkbox" id="actionBarCheckBox" className="d-inline-block" />
-            <Dropdown className="d-inline-block px-2">
-              <Dropdown.Toggle as={FontAwesomeIcon} icon={faEllipsis} role="button" />
-              <Dropdown.Menu>
-                <Dropdown.Header>Actions</Dropdown.Header>
-                <Dropdown.Item>Take an action</Dropdown.Item>
-                <Dropdown.Item className="text-bg-danger">
-                  <div className="d-flex w-100 justify-content-start align-items-center">
-                    <span className="me-3">
-                      <FontAwesomeIcon icon={faTrashAlt} />
-                    </span>
-                    <span>Delete</span>
-                  </div>
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Form.Group>
-        </Form>
-      </th>
+      <thead>
+        <tr>
+          <th className="text-bg-primary p-1 rounded-top" colSpan={4}>
+            <Form>
+              <Form.Group>
+                <Form.Check type="checkbox" id="actionBarCheckBox" className="d-inline-block" />
+                <Dropdown className="d-inline-block px-2">
+                  <Dropdown.Toggle as={FontAwesomeIcon} icon={faEllipsis} role="button" />
+                  <Dropdown.Menu>
+                    <Dropdown.Header>Actions</Dropdown.Header>
+                    <Dropdown.Item>Take an action</Dropdown.Item>
+                    <Dropdown.Item className="text-bg-danger">
+                      <div className="d-flex w-100 justify-content-start align-items-center">
+                        <span className="me-3">
+                          <FontAwesomeIcon icon={faTrashAlt} />
+                        </span>
+                        <span>Delete</span>
+                      </div>
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Form.Group>
+            </Form>
+          </th>
+        </tr>
+      </thead>
       <tbody>{mailRows}</tbody>
     </Table>
   );
 }
+
+MailTable.propTypes = {
+  mail: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      path: PropTypes.string.isRequired,
+      unread: PropTypes.bool.isRequired,
+      favorited: PropTypes.bool.isRequired,
+    })
+  ),
+};
+
+MailTable.defaultProps = {
+  mail: [],
+};
