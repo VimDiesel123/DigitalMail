a9ecce4bd48bce75d298040c345f2b8007a526b1
david <david@securescan.com>
2023-01-12 16:03:30 -0500

    Factor mail state related functions into custom useMail hook

--- App/src/routes/MailList/MailList.jsx
+++ App/src/routes/MailList/MailList.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React from 'react';
 import { Pagination } from 'react-bootstrap';
-import { useAuth0 } from '@auth0/auth0-react';
 import MailTable from './MailTable.jsx';
 import GreetingCard from './GreetingCard.jsx';
-import SearchContext from '../../SearchContext.jsx';
+import useMail from '../../useMail.js';
 
 export default function MailList() {
   const active = 1;
@@ -17,82 +16,14 @@ export default function MailList() {
     </Pagination.Item>
   ));
 
-  const [mails, setMail] = useState();
-  const { getAccessTokenSilently } = useAuth0();
+  const { mail, loading, markMailAsRead, setFavorite } = useMail();
 
-  const fetchMail = async () => {
-    const accessToken = await getAccessTokenSilently();
-
-    const response = await fetch(`/api/user/mail`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    const { mail } = await response.json();
-
-    return mail;
-  };
-
-  const markMailAsRead = async (mailId) => {
-    const accessToken = await getAccessTokenSilently();
-
-    const baseUrl = window.location.origin;
-    const url = new URL(`api/user/mail/${mailId}`, baseUrl);
-    url.searchParams.append('read', 'true');
-
-    const response = await fetch(url, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-
-    // TODO: (David) This is really inefficient and could cause problems. I'm refetching all the mail when only one is updated.
-    const updatedMail = await fetchMail();
-    setMail(updatedMail);
-
-    console.log(response);
-  };
-
-  const setFavorite = async (mailId, value) => {
-    const accessToken = await getAccessTokenSilently();
-
-    const baseUrl = window.location.origin;
-    const url = new URL(`api/user/mail/${mailId}`, baseUrl);
-    url.searchParams.append('favorite', value);
-
-    const response = await fetch(url, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-
-    // TODO: (David) This is really inefficient and could cause problems. I'm refetching all the mail when only one is updated.
-    const updatedMail = await fetchMail();
-    setMail(updatedMail);
-
-    console.log(response);
-  };
-
-  const { searchTerm } = useContext(SearchContext);
-
-  useEffect(() => {
-    async function updateMail() {
-      const mail = await fetchMail();
-      setMail(mail.filter((mailItem) => mailItem.sender.includes(searchTerm)));
-    }
-    updateMail();
-  }, [searchTerm]);
+  if (loading) return <div>Loading...</div>;
 
   return (
     <div className="d-flex flex-column">
       <GreetingCard />
-      <MailTable mail={mails} markMailAsRead={markMailAsRead} setFavorite={setFavorite} />
+      <MailTable mail={mail} markMailAsRead={markMailAsRead} setFavorite={setFavorite} />
       <div className="d-flex align-items-center border border-top-0 rounded-bottom">
         <Pagination className="ms-auto my-0 py-1">
           <Pagination.First />
---
+++ App/src/useMail.js
@@ -0,0 +1,85 @@
+import { useState, useEffect, useContext } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import SearchContext from './SearchContext.jsx';
+
+export default function useMail() {
+  // const [loading, setLoading] = useState();
+  const [mails, setMail] = useState();
+  const [loading, setLoading] = useState(true);
+  const { getAccessTokenSilently } = useAuth0();
+
+  const fetchMail = async () => {
+    setLoading(true);
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`/api/user/mail`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const { mail } = await response.json();
+    // This is to simulate long load times from the api.
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    setLoading(false);
+    return mail;
+  };
+
+  const markMailAsRead = async (mailId) => {
+    const accessToken = await getAccessTokenSilently();
+
+    const baseUrl = window.location.origin;
+    const url = new URL(`api/user/mail/${mailId}`, baseUrl);
+    url.searchParams.append('read', 'true');
+
+    const response = await fetch(url, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    // TODO: (David) This is really inefficient and could cause problems. I'm refetching all the mail when only one is updated.
+    const updatedMail = await fetchMail();
+    setMail(updatedMail);
+
+    console.log(response);
+  };
+
+  const setFavorite = async (mailId, value) => {
+    const accessToken = await getAccessTokenSilently();
+
+    const baseUrl = window.location.origin;
+    const url = new URL(`api/user/mail/${mailId}`, baseUrl);
+    url.searchParams.append('favorite', value);
+
+    const response = await fetch(url, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    // TODO: (David) This is really inefficient and could cause problems. I'm refetching all the mail when only one is updated.
+    const updatedMail = await fetchMail();
+    setMail(updatedMail);
+
+    console.log(response);
+  };
+
+  const { searchTerm } = useContext(SearchContext);
+
+  useEffect(() => {
+    async function updateMail() {
+      const mail = await fetchMail();
+      if (mail) setMail(mail.filter((mailItem) => mailItem.sender.includes(searchTerm)));
+    }
+    updateMail();
+  }, [searchTerm]);
+
+  return { loading, mail: mails, markMailAsRead, setFavorite };
+}
