b849562fd309a1d09a1f464956eab985bff5ef5b
david <david@securescan.com>
2023-01-13 13:58:21 -0500

    Add username to greeting card

--- App/src/routes/MailList/GreetingCard.jsx
+++ App/src/routes/MailList/GreetingCard.jsx
@@ -1,11 +1,13 @@
 import React, { useContext, useState } from 'react';
 import { Card, CloseButton, Collapse } from 'react-bootstrap';
+import { useAuth0 } from '@auth0/auth0-react';
 import UnreadContext from '../../UnreadContext.jsx';
 
 export default function GreetingCard() {
   const [visible, setVisible] = useState(true);
 
   const { unreadCount } = useContext(UnreadContext);
+  const { user } = useAuth0();
 
   return (
     <Collapse timeout={250} appear in={visible}>
@@ -13,7 +15,7 @@ export default function GreetingCard() {
         <Card bg="dark" border="info" text="light" className="w-75 my-3 mx-auto position-relative">
           <Card.Body>
             <Card.Title>
-              Welcome Back <b>BRO!</b>
+              Welcome Back <b>{user.name}!</b>
             </Card.Title>
             <Card.Text className="text-info">
               You have <b>{unreadCount}</b> unread mails.
